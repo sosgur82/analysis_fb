4e57e42c40839b197e04914c820e6cd09409d4eb
sosgur82 <sosgur82@gamil.com>
2017-10-13 14:59:20 +0900

    facebook crawler 단어 빈도수 체크완성

--- __main__.py
+++ __main__.py
@@ -1,7 +1,10 @@
 import collection
+import analyze
+
 pagename = 'jtbcnews'
 since = '2017-10-01'
 until = '2017-10-01'
+
 if __name__ == '__main__':
     items = [
         {'pagename':'jtbcnews', 'since':'2017-10-01', 'until':'2017-10-13'},
@@ -12,9 +15,13 @@ if __name__ == '__main__':
        resultfile = collection.crawling(**item)
        item['resultfile'] = resultfile
 
-    # analysis1
+    # analysis
     for item in items:
-        print(item['resultfile'])
+        data = analyze.json_to_str(item['resultfile'],'message')
+        item['count'] =  analyze.count_wordfreq(data)
+
 
     # visualization
-
+    for item in items:
+        count = item['count']
+        print(count)
