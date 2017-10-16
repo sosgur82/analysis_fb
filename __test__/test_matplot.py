0e2063abdf54ff3b78cc8445a77fb242deb1aedc
sosgur82 <sosgur82@gamil.com>
2017-10-16 14:56:01 +0900

    facebook 뉴스 크롤링 완성

--- __test__/test_matplot.py
+++ __test__/test_matplot.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.random import randn
 import matplotlib.pyplot as plt
-
+from matplotlib import font_manager, rc
 
 def ex1():
     # plt.plot([1, 2, 3, 4], [10, 20, 30, 40])
@@ -53,30 +53,36 @@ def ex5():
 
 
 def ex6():
-    fig, subplots = plt.subplots(2, 2)
+    fig, subplots = plt.subplots(2, 2, sharex=True, sharey=True)
     for i in range(2):
         for j in range(2):
-            subplots[i,j].hist(randn(100), bins=20, color='k', alpha=0.3)
+            subplots[i, j].hist(randn(100), bins=20, color='k', alpha=0.3)
 
     plt.subplots_adjust(wspace=0, hspace=0)
     plt.show()
 
 
 def ex7():
-    fig, subplots = plt.subplots(1,1)
-    subplots.plot([1,2,3,4], [10,20,30,40], 'ko-')
+    fig, subplots = plt.subplots(1, 1)
+    subplots.plot([1, 2, 3, 4], [10, 20, 30, 40], 'ko-')
     plt.show()
 
 
 def ex8():
     fig, subplots = plt.subplots(1, 1)
-    subplots.plot([1, 2, 3, 4], [10, 20, 30, 40], color='k', marker='o', linestyle='-')
+    subplots.plot(
+        [1, 2, 3, 4],
+        [10, 20, 30, 40],
+        color='#000000',
+        marker='o',
+        linestyle='solid')
     plt.show()
 
 
 def ex9():
     fig, subplots = plt.subplots(1, 1)
-    subplots.plot(randn(50).cumsum(), color='#34ff22', marker='o', linestyle='-')
+    # subplots.plot(randn(50).cumsum(), 'ko--')
+    subplots.plot(randn(50).cumsum(), color='#34ff22', marker='o', linestyle="-.")
     plt.show()
 
 
@@ -85,20 +91,79 @@ def ex10():
 
     fig, subplots = plt.subplots(1, 1)
     subplots.plot(data, color='#aaaaaa', linestyle='--', label='Default')
-    subplots.plot(data, 'k-', drawstyle='steps-mid', label='steps-mid')
+    subplots.plot(data, 'k-', drawstyle='steps-post', label='steps-mid')
 
     plt.legend(loc='best')
     plt.show()
 
 
+def ex11():
+    fig, subplots = plt.subplots(1, 1)
+    subplots.plot(randn(100).cumsum())
+
+    # subplots.set_xticks([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+    subplots.set_xticklabels(
+        ['pt0', 'pt1', 'pt2', 'pt3', 'pt4', 'pt5', 'pt6', 'pt7', 'pt8', 'pt9', 'pt10'],
+        rotation=30,
+        fontsize='small')
+    subplots.set_xlabel('Positions')
+    subplots.set_title('My First Matplotlib Plot')
+
+    plt.show()
+
+
+def ex12():
+    fig, subplots = plt.subplots(1, 1)
+    subplots.plot(randn(100).cumsum(), 'k', label='one')
+    subplots.plot(randn(100).cumsum(), 'k--', label='two')
+    subplots.plot(randn(100).cumsum(), 'k.', label='three')
+
+    plt.legend(loc='best')
+    plt.show()
+
+
+def ex13():
+    # font_options = {'family': 'Malgun Gothic'}
+    # plt.rc('font', **font_options)
+    # plt.rc('font', family='Malgun Gothic')
+    # plt.rc('axes', unicode_minus=False)
+
+    fig, subplots = plt.subplots(1, 1)
+    subplots.plot(randn(100).cumsum())
+
+    # subplots.set_xticks([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+    subplots.set_xticklabels(
+        ['pt0', 'pt1', 'pt2', 'pt3', 'pt4', 'pt5', 'pt6', 'pt7', 'pt8', 'pt9', 'pt10'],
+        rotation=30,
+        fontsize='small')
+    subplots.set_xlabel('포인트')
+    subplots.set_title('예제13 한글처리')
+
+    plt.savefig('ex13-plot.png', dpi=400, bbox_inches='tight')
+    # plt.savefig('ex13-plot.jpg', dpi=400, bbox_inches='tight')
+    plt.savefig('ex13-plot.pdf', dpi=400, bbox_inches='tight')
+    plt.savefig('ex13-plot.svg', dpi=400, bbox_inches='tight')
+
+    plt.show()
+
+
+def ex14():
+    fontfile = 'c:/windows/fonts/malgun.ttf'
+    fontname = font_manager.FontProperties(fname=fontfile).get_name()
+    print(fontname)
+
+
 if __name__ == '__main__':
+    # ex14()
+    ex13()
+    # ex12()
+    # ex11()
+    # ex10()
     # ex1()
     # ex2()
     # ex3()
     # ex4()
-    # ex5()
     # ex6()
     # ex7()
     # ex8()
     # ex9()
-     ex10()
