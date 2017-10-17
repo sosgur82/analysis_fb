a968fc729b0bad7ddeb76f20034d36423d00042f
sosgur82 <sosgur82@gamil.com>
2017-10-17 11:52:07 +0900

    태그클라우드 모듈 추가 및 http 테스트

--- __test__/test_httprequesthandler.py
+++ __test__/test_httprequesthandler.py
@@ -1,11 +1,42 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
 
 PORT = 8000
 
 
 class TestHTTPRequestHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
-        print(self.path)
+        index = self.path.find('?')
+        req_url = self.path if index == -1 else self.path[:self.path.index('?')]
+
+        if req_url != '/graph':
+            self.send_error(404, 'File Not Found:' + req_url)
+            return
+
+        handler_name = 'ex' + self.get_params('ex')
+        if handler_name not in TestHTTPRequestHandler.__dict__:
+            self.send_error(404, 'File Not Found:' + req_url)
+            return
+
+        TestHTTPRequestHandler.__dict__[handler_name](self)
+
+    def get_params(self, name):
+        index = self.path.find('?')
+        if index == -1:
+            return
+
+        qs = self.path[self.path.index('?')+1:]
+        params = parse_qs(qs)
+        values = params.get(name)
+
+        return None if values is None else values.pop()
+
+    def ex1(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write("<h1>안녕하세요</h1>".encode('utf-8'))
 
 
 httpd = HTTPServer(('', PORT), TestHTTPRequestHandler)
